af0a633f0394e21e8886c0633c7d819f295a9f30
Park Chan-Min <walkinpcm@gmail.com>
2015-07-06 17:18:36 +0900

    Update autorun_opentsdb.sh

--- autorun_opentsdb.sh
+++ autorun_opentsdb.sh
@@ -8,9 +8,8 @@ sudo env COMPRESSION=NONE HBASE_HOME=/usr/local/hbase-1.0.1.1 ./src/create_table
 
 tsdtmp=${TMPDIR-'/usr/local/data'}/tsd
 
-screen -dmS tsdb
-screen -list
-screen -r tsdb
+#screen -dmS tsdb
+#screen -list
+#screen -r tsdb
 
-#if the screen is changed, write the below. and get out by ctrl+a+d
-#sudo ./build/tsdb tsd --port=4242 --staticroot=build/staticroot --cachedir=/usr/local/data --auto-metric
+sudo ./build/tsdb tsd --port=4242 --staticroot=build/staticroot --cachedir=/usr/local/data --auto-metric &
