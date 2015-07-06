471f06211be1eef163c80e3d78dc24819a6f1d5f
Park Chan-Min <walkinpcm@gmail.com>
2015-07-06 16:51:42 +0900

    Create autorun_opentsdb.sh

---
+++ autorun_opentsdb.sh
@@ -0,0 +1,15 @@
+cd /usr/local/hbase-1.0.1.1/
+cd bin/
+sudo ./start-hbase.sh 
+
+cd /usr/local/opentsdb
+
+sudo env COMPRESSION=NONE HBASE_HOME=/usr/local/hbase-1.0.1.1 ./src/create_table.sh
+
+tsdtmp=${TMPDIR-'/usr/local/data'}/tsd
+
+screen -dmS tsdb
+screen -list
+screen -r tsdb
+
+sudo ./build/tsdb tsd --port=4242 --staticroot=build/staticroot --cachedir=/usr/local/data --auto-metric
