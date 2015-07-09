541acaa1e32c98f70f324f42adc8ca41f030a13c
Park Chan-Min <walkinpcm@gmail.com>
2015-07-09 13:47:51 +0900

    Update hue_control.py

--- hue/hue_control.py
+++ hue/hue_control.py
@@ -1,10 +1,46 @@
 import time
+import random
 from hue import *
 
 def main():
-  print getState(2)
+  lights=[1,2,3]
+  
+  for lightnum in lights:
+    print getState(lightnum)
   time.sleep(2)
+  
+  for lightnum in lights:
+    hue_putHue(lightnum,0)
+    hue_putSat(lightnum,0)
 
+  for lightnum in lights:
+    hue_off(lightnum)
+    time.sleep(2)
+  time.sleep(3)
+  
+  for lightnum in lights:
+    hue_effect(lightnum, 1)
+  time.sleep(15)
+  for lightnum in lights:
+    hue_effect(lightnum, 0)
+  
+  for lightnum in lights:
+    hue_off(lightnum)
+  time.sleep(2)
+  
+  for lightnum in lights:
+    hue_alert(lightnum, 2)
+  time.sleep(5)
+  for lightnum in lights:
+    hue_alert(lightnum, 0)
+  
+  for lightnum in lights:
+    random_bri = random.randrange(0,65535) #0~65535 사이의 정수 랜덤으로 출력
+    hue_putHue(lightnum, random_bri)
+  
+  for lightnum in lights:
+    print getState(lightnum)
+  """
   hue_off(2)
   time.sleep(5)
   
@@ -29,6 +65,7 @@ def main():
   hue_effect(2, 0)
   print getState(2)
   time.sleep(5)
+  """
   
 if __name__ == '__main__':
   main()
