ab079269ec037521118932cf5a94b59a7821af75
Park Chan-Min <walkinpcm@gmail.com>
2015-07-09 12:44:56 +0900

    Create hue_control.py

---
+++ hue/hue_control.py
@@ -0,0 +1,17 @@
+import time
+from hue import *
+
+def main():
+  print getState(2)
+  time.sleep(2)
+"""  
+  hue_off(2)
+  time.sleep(5)
+  
+  hue_on(2)
+  time.sleep(5)
+  
+  hue_putHue(2, 43210)
+"""
+if __name__ == '__main__':
+  main()
