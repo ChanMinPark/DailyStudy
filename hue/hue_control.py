a1ae161baa0fdb0ae89d18401f451875f468495f
Park Chan-Min <walkinpcm@gmail.com>
2015-07-09 12:56:14 +0900

    Update hue_control.py

--- hue/hue_control.py
+++ hue/hue_control.py
@@ -4,7 +4,7 @@ from hue import *
 def main():
   print getState(2)
   time.sleep(2)
-"""  
+
   hue_off(2)
   time.sleep(5)
   
@@ -12,6 +12,7 @@ def main():
   time.sleep(5)
   
   hue_putHue(2, 43210)
-"""
+  time.sleep(5)
+  
 if __name__ == '__main__':
   main()
