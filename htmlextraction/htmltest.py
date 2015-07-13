cd5eed70f79d413b8ad0624e90fcf88346f8255e
ChanMinPark <walkinpcm@gmail.com>
2015-07-13 13:51:00 +0900

    updata

--- htmlextraction/htmltest.py
+++ htmlextraction/htmltest.py
@@ -1,12 +1,13 @@
 import urllib2
 
 if __name__ == '__main__':
-	page = urllib2.urlopen("http://www.example.com")
+	#page = urllib2.urlopen("http://www.example.com")
+	page = urllib2.urlopen("http://sports.news.naver.com/record/index.nhn?uCategory=kbaseball&category=kbo")
 	text = page.read()
 	print text
 
-	where = text.find("This domain is")
+	#where = text.find("This domain is")
 
-	print where
-	print text[where:where+10]
+	#print where
+	#print text[where:where+10]
 
