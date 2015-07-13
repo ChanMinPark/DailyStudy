4ce6af1a48297001f0a70e44fa606d550eb59e14
Park Chan-Min <walkinpcm@gmail.com>
2015-07-13 12:54:56 +0900

    Update flow_lcd.py

--- LCD_TH/flow_lcd.py
+++ LCD_TH/flow_lcd.py
@@ -6,13 +6,18 @@ def main():
   # Initialise display
   lcd_init()
   
-  content = "Hello World! Have a nice day~"
+  content = "Hello World! Have a nice day^^  "
+  content2 = "This is ChanMin. Hello everyone. "
   while True:
     lcd_string('%s' %(content), LCD_LINE_1,1)
+    lcd_string('%s' %(content2), LCD_LINE_2,3)
     time.sleep(1)
     temp_content = content[:1]
     content = content[1:]
     content += temp_content
+    temp_content2 = content2[-1:]
+    temp_content2 += content2[:-1]
+    content2 = temp_content2
     
 
 if __name__ == '__main__':
