966311aeb757e11dbd705c2c4232129ab62946cb
Park Chan-Min <walkinpcm@gmail.com>
2015-07-13 14:02:35 +0900

    Create getDatafromWeb.py

---
+++ htmlextraction/getDatafromWeb.py
@@ -0,0 +1,12 @@
+import urllib2
+
+def getBaseBallRank():
+  page = urllib2.urlopen("http://sports.news.naver.com/record/index.nhn?uCategory=kbaseball&category=kbo")
+	text = page.read()
+	#print text
+	
+	print text.split('var jsonTeamRecord')[1].split('var teamCount')[0]
+
+if __name__ == '__main__':
+	getBaseBallRank()
+	
