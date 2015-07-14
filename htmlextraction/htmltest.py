27bf30fa3fabdf9afdbc73225d5ce52149e875e6
Park Chan-Min <walkinpcm@gmail.com>
2015-07-14 12:42:54 +0900

    Update htmltest.py

--- htmlextraction/htmltest.py
+++ htmlextraction/htmltest.py
@@ -1,13 +1,15 @@
+#Author : ChanMin Park, https://github.com/ChanMinPark
+#Abstract : Simple code for getting a web page using urllib2 module
+
 import urllib2
 
 if __name__ == '__main__':
-	#page = urllib2.urlopen("http://www.example.com")
-	page = urllib2.urlopen("http://sports.news.naver.com/record/index.nhn?uCategory=kbaseball&category=kbo")
+	page = urllib2.urlopen("http://www.example.com")
 	text = page.read()
 	print text
-
+	
+	# If you want to know the location of certain text, use below code.
 	#where = text.find("This domain is")
-
 	#print where
 	#print text[where:where+10]
 
