2cc70ed5c2eb667c8b64415ea7916147443f1788
Park Chan-Min <walkinpcm@gmail.com>
2015-07-14 16:50:00 +0900

    Update musictest.py

--- htmlextraction/musictest.py
+++ htmlextraction/musictest.py
@@ -1,9 +1,9 @@
 import pygame
 
 pygame.mixer.init()
-song = pygame.mixer.music.load("samsung_song.mp3")
+pygame.mixer.music.load("samsung_song.mp3")
 #clock = pygame.time.Clock()
-song.play()
+pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:  #True:
     continue    #clock.tick(60)
 pygame.mixer.quit()
