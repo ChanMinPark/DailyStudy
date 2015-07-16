4a4b7f77f93cddc93a8cfddf83ca95e6916c3311
Park Chan-Min <walkinpcm@gmail.com>
2015-07-16 18:45:58 +0900

    Update lcd_moving.py

--- lcd_moving/lcd_moving.py
+++ lcd_moving/lcd_moving.py
@@ -9,6 +9,13 @@ import curses
 characterLine=0
 characterPos=0
 
+#Position of Target
+targetPos=0
+
+#Line Text
+line_f="                "
+line_s="                "
+
 def main():
   # get the curses screen window
   stdscr = curses.initscr()
@@ -19,6 +26,12 @@ def main():
   # map arrow keys to special values
   stdscr.keypad(True)
   
+  initWord()
+  time.sleep(5)
+  line_f="                "
+  printToLCD()
+  
+  
   finally:
     # shut down cleanly
     curses.nocbreak(); stdscr.keypad(0); curses.echo()
@@ -26,18 +39,62 @@ def main():
     
 def initWord():
   #write codes
+  line_f = " Get the Target "
+  line_s = "       @        "
+  characterLine=2
+  characterPos=7
+  
+  printToLCD()
 
 def pressUpKey():
   #write codes
+  if characterLine != 1:
+    line_f[characterPos] = line_s[characterPos]
+    line_s[characterPos] = " "
+    characterLine = characterLine - 1
+    
+    printToLCD()
   
 def pressDownKey():
   #write codes
+   if characterLine != 2:
+    line_s[characterPos] = line_f[characterPos]
+    line_f[characterPos] = " "
+    characterLine = characterLine + 1
+    
+    printToLCD()
   
 def pressRightKey():
   #write codes
+  if characterPos != 15:
+    if characterLine == 1:
+      line_f[characterPos+1] = line_f[characterPos]
+      line_f[characterPos] = " "
+    else:
+      line_s[characterPos+1] = line_s[characterPos]
+      line_s[characterPos] = " "
+    characterPos = characterPos + 1
+    printToLCD()
 
 def pressLeftKey():
   #write codes
+  if characterPos != 0:
+    if characterLine == 1:
+      line_f[characterPos-1] = line_f[characterPos]
+      line_f[characterPos] = " "
+    else:
+      line_s[characterPos-1] = line_s[characterPos]
+      line_s[characterPos] = " "
+    characterPos = characterPos - 1
+    printToLCD()
   
 def printToLCD():
+  #write codes
+  lcd_string('%s' %(line_f), LCD_LINE_1,1)
+  lcd_string('%s' %(line_s), LCD_LINE_2,1)
+  
+def changeColor():
+  #write codes
+
+def locateTarget():
   #write codes
