441954bc708470094f764463bd74d9f3b9971d21
ChanMinPark <walkinpcm@gmail.com>
2015-07-31 15:07:12 +0900

    update myapp

---
+++ web2py/myapp/modules/temp_humi_co2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from gluon import *
+import sys
+import smbus
+import time
+import requests
+import json
+
+SHT20_ADDR = 0x40       # SHT20 register address
+SHT20_CMD_R_T = 0xF3    # no hold Master Mode (Temperature)
+SHT20_CMD_R_RH = 0xF5   # no hold Master Mode (Humidity)
+SHT20_CMD_RESET = 0xFE  # soft reset
+
+bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+def reading(v):
+    bus.write_quick(SHT20_ADDR)
+    if v == 1:
+        bus.write_byte(SHT20_ADDR, SHT20_CMD_R_T)
+    elif v == 2:
+        bus.write_byte(SHT20_ADDR, SHT20_CMD_R_RH)
+    else:
+        return False
+        
+    time.sleep(.1)
+    
+    b = (bus.read_byte(SHT20_ADDR)<<8)
+    b += bus.read_byte(SHT20_ADDR)
+    return b
+
+def calc(temp, humi):
+    tmp_temp = -46.85 + 175.72 * float(temp) / pow(2,16)
+    tmp_humi = -6 + 125 * float(humi) / pow(2,16)
+
+    return tmp_temp, tmp_humi
+
+
+def main():
+  lcd_init()
+  print "Hello ChanMin"
+  
+  while True:
+    #Write Temp/Humi getting code and printing code
+    temp = reading(1)
+    humi = reading(2)
+    if not temp or not humi:
+        print "register error"
+        break
+    value = calc(temp, humi)
+    #print "temp : %s\thumi : %s" % (value[0], value[1])
+    #lcd_string('Temp : %s' % (value[0]),LCD_LINE_1,1)
+    #lcd_string('Humi : %s' % (value[1]),LCD_LINE_2,1)
+    time.sleep(1)
+  
+  return value
