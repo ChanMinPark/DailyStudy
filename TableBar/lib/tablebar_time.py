46d177eb9268ee7c1cc21dc7ac0f80bb97408cc1
Park Chan-Min <walkinpcm@gmail.com>
2015-08-03 14:59:06 +0900

    Update tablebar_time.py

--- TableBar/lib/tablebar_time.py
+++ TableBar/lib/tablebar_time.py
@@ -21,10 +21,10 @@ def getData():
   elif now.tm_wday == 6:
     wday = "Sun"
     
-  r_date = "%d-%d-%d-%s"%(now.tm_year, now.tm_mon, now.tm_mday, wday)
+  r_date = "%4d-%2d-%2d-%s"%(now.tm_year, now.tm_mon, now.tm_mday, wday)
   return r_date
 
 def getTime():
   now = getNow()
-  r_time = "%d:%d:%d"%(now.tm_hour, now.tm_min, now.tm_sec)
+  r_time = "%2d:%2d:%2d"%(now.tm_hour, now.tm_min, now.tm_sec)
   return r_time
