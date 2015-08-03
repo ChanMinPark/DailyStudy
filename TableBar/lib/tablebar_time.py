c633954e542400cdb08fb8622085b6f47347691b
Park Chan-Min <walkinpcm@gmail.com>
2015-08-03 14:58:01 +0900

    Update tablebar_time.py

--- TableBar/lib/tablebar_time.py
+++ TableBar/lib/tablebar_time.py
@@ -1,5 +1,3 @@
-#-*-coding:utf-8-*-
-
 import time
 
 def getNow():
@@ -9,24 +7,24 @@ def getNow():
 def getData():
   now = getNow()
   if now.tm_wday == 0:
-    wday = "월"
+    wday = "Mon"
   elif now.tm_wday == 1:
-    wday = "화"
+    wday = "Tue"
   elif now.tm_wday == 2:
-    wday = "수"
+    wday = "Wed"
   elif now.tm_wday == 3:
-    wday = "목"
+    wday = "Thu"
   elif now.tm_wday == 4:
-    wday = "금"
+    wday = "Fri"
   elif now.tm_wday == 5:
-    wday = "토"
+    wday = "Sat"
   elif now.tm_wday == 6:
-    wday = "일"
+    wday = "Sun"
     
-  r_date = "%d년 %d월 %d일 %s요일"%(now.tm_year, now.tm_mon, now.tm_mday, wday)
+  r_date = "%d-%d-%d-%s"%(now.tm_year, now.tm_mon, now.tm_mday, wday)
   return r_date
 
 def getTime():
   now = getNow()
-  r_time = "%d시 %d분 %d초"%(now.tm_hour, now.tm_min, now.tm_sec)
+  r_time = "%d:%d:%d"%(now.tm_hour, now.tm_min, now.tm_sec)
   return r_time
