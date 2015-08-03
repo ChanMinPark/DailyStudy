f142baffb094a3d1bbf48f2e61909380613c461d
Park Chan-Min <walkinpcm@gmail.com>
2015-08-03 14:49:59 +0900

    Update tablebar.py

--- TableBar/tablebar.py
+++ TableBar/tablebar.py
@@ -1,4 +1,21 @@
-import tablebar_time
+sys.path.append("./lib")
+from lcd import *
+from tablebar_time import *
 
 def main():
+  # Initialise display
+  lcd_init()
   
+  # Display date & time
+  lcd_string(getData(), LCD_LINE_1, 2)
+  lcd_string(getTime(), LCD_LINE_2, 2)
+
+if __name__ == '__main__':
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    lcd_byte(0x01, LCD_CMD)
+    lcd_string("Goodbye!",LCD_LINE_1,2)
+    GPIO.cleanup()
