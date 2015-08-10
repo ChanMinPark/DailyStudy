11578e3292553a028d2d35f4890fc62dfe115eb2
Park Chan-Min <walkinpcm@gmail.com>
2015-08-10 12:42:33 +0900

    Update tablebar_calender.py

--- TableBar/lib/tablebar_calender.py
+++ TableBar/lib/tablebar_calender.py
@@ -9,26 +9,39 @@ def getWeek():
   line_1 = returnMonth(now.tm_mon) + "#Sun Mon The Wed Thu Fri Sat "
   line_2 = "%02d #"%(now.tm_mday)
   
+  weekdays[wday] = today
+  
   temp = wday
-  while temp>=0
-
-
-def returnWday(a):
-  if a == 0:
-    return "Sun"
-  elif a == 1:
-    return "Mon"
-  elif a == 2:
-    return "Tue"
-  elif a == 3:
-    return "Wed"
-  elif a == 4:
-    return "Thu"
-  elif a == 5:
-    return "Fri"
-  elif a == 6:
-    return "Sat"
-
+  while temp > 0:
+    temp = temp - 1
+    weekdays[temp] = weekdays[temp+1] - 1
+    
+  temp = wday
+  while temp < 6:
+    temp = temp + 1
+    weekdays[temp] = weekdays[temp-1] + 1
+  
+  for dd in weekdays:
+    if (now.tm_mon-1) in [1,3,5,7,8,10,12]:
+      if dd < 1:
+        dd = 31-dd
+    else:
+      if dd < 1:
+        dd = 30-dd
+        
+    if now.tm_mon in [1,3,5,7,8,10,12]:
+      if dd > 31:
+        dd = dd-31
+    else:
+      if dd > 30:
+        dd = dd-30
+        
+    line_2 = line_2 + " %02d "%(dd)
+  
+  lines[0] = line_1
+  lines[1] = line_2
+  
+  return lines
 
 def returnMonth(a):
   if a == 1:
@@ -55,5 +68,3 @@ def returnMonth(a):
     return "Nov"
   elif a == 12:
     return "Dec"
-
-  #(작성중...)
