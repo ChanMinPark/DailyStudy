469d0ab5ebb69eab9c665cb0d91ad3f290b8e495
Park Chan-Min <walkinpcm@gmail.com>
2015-08-11 13:31:06 +0900

    Update tablebar.py

--- TableBar/tablebar.py
+++ TableBar/tablebar.py
@@ -3,16 +3,17 @@ sys.path.append("./lib")
 from lcd import *
 from tablebar_time import *
 from tablebar_calender import *
+from tablebar_weather import *
 
 def main():
   # Initialise display
   lcd_init()
-  
+  """ 
   while True:
     # Display date & time
     lcd_string(getData(), LCD_LINE_1, 2)
     lcd_string(getTime(), LCD_LINE_2, 2)
-  """  
+    
   while True:
     # Display calender
     plines = getWeek()
@@ -25,6 +26,13 @@ def main():
       plines[1] = plines[1][:4]+plines[1][8:]
       cycle = cycle - 1
   """
+  
+  while True:
+    data = {}
+    data = getWeather()
+    lcd_string("Now : "+data['now_temp']+"℃, "+data['now_weather'], LCD_LINE_1, 2)
+    lcd_string("1h : "+data['one_later']+", 2h : "+data['two_later'], LCD_LINE_2, 2)
+    time.sleep(5)
 
 if __name__ == '__main__':
   try:
