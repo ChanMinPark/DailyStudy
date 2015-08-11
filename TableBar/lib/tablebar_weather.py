692242ed34443e1ad75b7330875393455923251e
Park Chan-Min <walkinpcm@gmail.com>
2015-08-11 16:24:07 +0900

    Update tablebar_weather.py

--- TableBar/lib/tablebar_weather.py
+++ TableBar/lib/tablebar_weather.py
@@ -3,7 +3,7 @@
 import urllib2
 
 def getWeather():
-  pre_url = "http://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ie=utf8&query=%EB%82%A0%EC%94%A8+"+getLocation()
+  pre_url = "http://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q=%EB%82%A0%EC%94%A8+"+getLocation()
   print pre_url
   req = urllib2.Request(pre_url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; Windows; U; MSIE 9.0; Windows NT 9.0; en-US) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'})
   #url = urllib2.quote(pre_url, '/:')
