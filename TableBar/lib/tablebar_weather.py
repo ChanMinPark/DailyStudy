1279464adc39957fb5f836ec9be11f298a274e59
Park Chan-Min <walkinpcm@gmail.com>
2015-08-11 13:18:07 +0900

    Create tablebar_weather.py

---
+++ TableBar/lib/tablebar_weather.py
@@ -0,0 +1,20 @@
+import urllib2
+
+def getWeather():
+  page = urllib2.urlopen("view-source:http://search.naver.com/search.naver?where=nexearch&query=%EB%82%A0%EC%94%A8+"+getLocation()+"&sm=top_hty&fbm=1&ie=utf8")
+  text = page.read()
+	
+	data = {}
+  data['now'] = text.split('현재,1시간 예보')[1].split('3시간 예보')[0].split('<em>')[1].split('<span>')[0]
+  data['one_later'] = text.split('현재,1시간 예보')[1].split('3시간 예보')[0].split('<em>')[2].split('<p>')[1].split('</p>')[0]
+  data['two_later'] = text.split('현재,1시간 예보')[1].split('3시간 예보')[0].split('<em>')[3].split('<p>')[1].split('</p>')[0]
+  
+  return data
+  
+def getLocation():
+  page = urllib2.urlopen("https://169.254.1.89:8000/myapp/default/project_tablebar_setting")
+  text = page.read()
+	
+  set_localation = text.split('설정된 지역 : ')[1].split('</td>')[0]
+  
+  return set_localation
