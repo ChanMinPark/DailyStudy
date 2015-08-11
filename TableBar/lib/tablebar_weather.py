4aa9382d6b75f66aabee17fbdae682ea9b94949b
Park Chan-Min <walkinpcm@gmail.com>
2015-08-11 16:37:51 +0900

    Update tablebar_weather.py

--- TableBar/lib/tablebar_weather.py
+++ TableBar/lib/tablebar_weather.py
@@ -3,19 +3,18 @@
 import urllib2
 
 def getWeather():
-  pre_url = "http://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q=%EB%82%A0%EC%94%A8+"+getLocation()
+  pre_url = "http://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ie=utf8&query=%EB%82%A0%EC%94%A8+"+getLocation()
   print pre_url
   req = urllib2.Request(pre_url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; Windows; U; MSIE 9.0; Windows NT 9.0; en-US) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'})
   #url = urllib2.quote(pre_url, '/:')
   #print url
   page = urllib2.urlopen(req)
   text = page.read()
-  print text
+  #print text
   
   data = {}
-  print text.split('c_body')[1].split('tldw_tbl')[0]
-  data['now_temp'] = text.split('c_body')[1].split('tldw_tbl')[0].split('<em>')[1].split('<span>')[0]
-  print data['now_temp']
+  #data['now_temp'] = text.split('c_body')[1].split('tldw_tbl')[0].split('<em>')[1].split('<span>')[0]
+  data['now_temp'] = text.split('현재,1시간 예보')[1].split('3시간 예보')[0].split('<em>')[1].split('<span>')[0]
   data['now_weather'] = text.split('현재,1시간 예보')[1].split('3시간 예보')[0].split('<strong>')[1].split('</strong>')[0]
   data['one_later'] = text.split('현재,1시간 예보')[1].split('3시간 예보')[0].split('<em>')[2].split('<p>')[1].split('</p>')[0]
   data['two_later'] = text.split('현재,1시간 예보')[1].split('3시간 예보')[0].split('<em>')[3].split('<p>')[1].split('</p>')[0]
