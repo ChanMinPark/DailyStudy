63ba23abc8cec5e2802c6e83f5fe16637fcb2f26
Park Chan-Min <walkinpcm@gmail.com>
2015-08-18 15:03:41 +0900

    Update tablebar.py

--- web2py/myapp/cron/tablebar.py
+++ web2py/myapp/cron/tablebar.py
@@ -9,32 +9,41 @@ def main():
     lcd_init()
     whiteLCDon()
     
-    r_time = 0
-    while True:#r_time >= 0:
-        lcd_string("Cron Test", LCD_LINE_1, 2)
-        lcd_string("%d, %d"%(getTask(), r_time), LCD_LINE_2, 2)
-        r_time = r_time + 1
-        time.sleep(1)
+    # Display time information
+    cycle = 10
+    while getTask() == 0:
+    	if cycle == 0:
+    	    setLock(False)
+    	    setTask(1)
+    	    break
+    	if getLock() == False:
+    	    setLock(True)
+    	    # Display date & time
+    	    lcd_string(getData(), LCD_LINE_1, 2)
+    	    lcd_string(getTime(), LCD_LINE_2, 2)
+    	    cycle = cycle - 1
+    	    time.sleep(1)
     
-    """ 
-    while True:
-        # Display date & time
-        lcd_string(getData(), LCD_LINE_1, 2)
-        lcd_string(getTime(), LCD_LINE_2, 2)
-    
-    while True:
+    # Display date information
+    while getTask() == 1:
         # Display calender
         plines = getWeek()
         cycle = 5
-        while cycle > 0:
-            time.sleep(2)
-            lcd_string(plines[0], LCD_LINE_1, 1)
-            lcd_string(plines[1], LCD_LINE_2, 1)
-            plines[0] = plines[0][:4]+plines[0][8:]
-            plines[1] = plines[1][:4]+plines[1][8:]
-            cycle = cycle - 1
+        if getLock() == False:
+            setLock(True)
+            while cycle > 0:
+                time.sleep(2)
+                lcd_string(plines[0], LCD_LINE_1, 1)
+                lcd_string(plines[1], LCD_LINE_2, 1)
+                plines[0] = plines[0][:4]+plines[0][8:]
+                plines[1] = plines[1][:4]+plines[1][8:]
+                cycle = cycle - 1
+            setLock(False)
+        if cycle == 0:
+            setTask(0)
+            break
   
-    
+    """
     while True:
         data = {}a
         data = getWeather()
